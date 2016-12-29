20aa3d02fb0be35452e7b81d4e9c028f3aba7bc3
boknows <lboland@fool.com>
2016-12-29 15:51:05 -0500

    progress on ui

---
+++ map/actions.py
@@ -0,0 +1,17 @@
+def attack(map, tile_acting, tile_effected):
+    tile_effected.units -= 1
+    if tile_effected.units == 0:
+        tile_effected.owner = tile_acting.owner
+    tile_acting.save()
+    tile_effected.save()
+    return tile_acting, tile_effected
+
+def move(map, tile_acting, tile_effected, units_to_move):
+    if tile_acting.owner == tile_effected.owner:
+        tile_acting.units -= units_to_move
+        tile_effected.units += units_to_move
+        tile_acting.save()
+        tile_effected.save()
+        return tile_acting, tile_effected
+    else:
+        raise Exception('Units can only be moved between tiles of the same owner')
